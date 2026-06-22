98274283592191cf2f8f414f93f821188c8802cb
Julio Cezar Oliveira de Xerez <jcoxerez@gmail.com>
2026-06-22 20:41:05 -0300

    primeiro commit

---
+++ FRONTEND/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="">
+      <span className='text-red-500 text-xl'>paia</span>
+    </div>
+    </>
+  )
+}
+
+export default App
